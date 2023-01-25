b6505e3cb40123c152fe283efc0106ea6c24e558
Angela-GM <angelagm0112@gmail.com>
2023-01-25 12:05:54 +0100

    Se ha corregido la section reviews, empezado la secction services, y conectado el archivo js con el index

--- resources/js/index.js
+++ resources/js/index.js
@@ -1,1 +1,17 @@
 /* Crea tu propia lógica para hacer fetch de 3 posts distintos y enseñarlos en la homepage con DOM manipulation */
+
+console.log("Hola Mundo");
+
+// Ejelo 003 - llamando una external PUBLIC API - jsonplaceholder
+let getExternalApi = () => {
+  fetch("https://jsonplaceholder.typicode.com/posts?_start=0&_end=3")
+    .then((response) => response.json())
+    .then((respuestas) => {
+      console.table(respuestas);
+    })
+    .catch((error) => console.log(error));
+};
+
+getExternalApi();
+
+//GET /posts?_start=20&_end=30
